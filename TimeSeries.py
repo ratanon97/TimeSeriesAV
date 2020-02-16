81bdef3d1663d949ca18012dbd080858471a2f36
Tap Suksumrun <ratanon97@users.noreply.github.com>
2020-02-16 17:29:49 +0000

    Initialising Python code

---
+++ TimeSeries.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+#Time Series Forecasting Using Python
+#https://courses.analyticsvidhya.com/courses/take/creating-time-series-forecast-using-python/texts/6133032-getting-the-system-ready-and-loading-data
+#-------------------------------------------------------------------------------
+#Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import os
+import re
+#-------------------------------------------------------------------------------
+#Set working directory 
+print(os.getcwd())
+os.chdir("C:\\Users\\KOMSUN\\Documents\\GitHub\\TimeSeriesAV\\Data Sources")
+#-------------------------------------------------------------------------------
